62bd86ab964d91a173548fb5ee3588563111789f
DanielFerreira <daniel18mtm@gmail.com>
2023-09-13 11:17:42 -0300

    initial commit

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,12 @@
-// Remova os comentários a medida que for implementando as funções
-
-export const searchCities = (/* term */) => {
-//   seu código aqui
+const APIKEY = import.meta.env.VITE_TOKEN;
+export const searchCities = async (term) => {
+  const resposta = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${APIKEY}&q=${term}`);
+  const dado = await resposta.json();
+  if (dado.length === 0) {
+    alert('Nenhuma cidade encontrada');
+    return [];
+  }
+  return dado;
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
