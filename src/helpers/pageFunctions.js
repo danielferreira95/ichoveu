cf0f30fafa628dc9f26213e618131188899736af
DanielFerreira <daniel18mtm@gmail.com>
2023-09-13 12:13:36 -0300

    Requisito 2

--- src/helpers/pageFunctions.js
+++ src/helpers/pageFunctions.js
@@ -1,4 +1,4 @@
-import { searchCities } from './weatherAPI';
+import { getWeatherByCity, searchCities } from './weatherAPI';
 
 /**
  * Cria um elemento HTML com as informações passadas
@@ -110,12 +110,13 @@ export function createCityElement(cityInfo) {
 /**
  * Lida com o evento de submit do formulário de busca
  */
-export function handleSearch(event) {
+export async function handleSearch(event) {
   event.preventDefault();
   clearChildrenById('cities');
 
   const searchInput = document.getElementById('search-input');
   const searchValue = searchInput.value;
-  searchCities(searchValue);
+  const retorno = await searchCities(searchValue);
+  retorno.forEach(async (retu) => getWeatherByCity(retu.url));
   // seu código aqui
 }
--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -4,11 +4,19 @@ export const searchCities = async (term) => {
   const dado = await resposta.json();
   if (dado.length === 0) {
     alert('Nenhuma cidade encontrada');
-    return [];
   }
   return dado;
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
+export const getWeatherByCity = async (URL_CIDADE) => {
 //   seu código aqui
+  const resposta = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${APIKEY}&q=${URL_CIDADE}`);
+  const dado = await resposta.json();
+
+  const obj = {
+    temp: dado.current.temp_c,
+    condition: dado.current.condition.text,
+    icon: dado.current.condition.icon,
+  };
+  return obj;
 };
